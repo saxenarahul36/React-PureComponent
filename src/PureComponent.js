3904d26b9b2aa3ba7ee22d66af2230459d16282c
Rahul Saxena <saxenarahul36@gmail.com>
2019-08-04 16:09:09 +0530

    Code changes

--- src/PureComponent.js
+++ src/PureComponent.js
@@ -1,16 +1,6 @@
 import React from "react";
 
 export default class PureComponent extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todos: [
-        { title: "take out the trash", done: false, notes: ["boring"] },
-        { title: "walk dog", done: true, notes: ["exercise"] },
-        { title: "read about React", done: false, notes: ["fun!"] }
-      ]
-    };
-  }
   componentDidMount() {
     console.log("componentDidMount PureComponent");
   }
